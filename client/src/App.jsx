b46593ec2ac0c96c9d5394ccdca3476c03f13fad
Pradeep dhangar <pradeepdhangar37@gmail.com>
2024-07-23 22:07:50 +0530

    create and run the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,19 +6,21 @@ import { SignIn } from './pages/SignIn'
 import { SignUp } from './pages/SignUp'
 import { Dashboard } from './pages/Dashboard'
 import { Project } from './pages/Project'
+import { Header } from './components/Header'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
    <BrowserRouter>
+   <Header/>
    <Routes>
     <Route path='/' element={<Home/>}/>
     <Route path='/about' element={<About/>}/>
     <Route path='/sign-in' element={<SignIn/>}/>
     <Route path='/sign-up' element={<SignUp/>}/>
     <Route path='/dashboard' element={<Dashboard/>}/>
-    <Route path='projets' element={<Project/>}/>
+    <Route path='projects' element={<Project/>}/>
 
 
    </Routes>
