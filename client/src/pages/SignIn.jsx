662ff1ce4dbc8de80fdd84f7f56f231f57b9a0de
Pradeep dhangar <pradeepdhangar37@gmail.com>
2024-07-25 21:42:44 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,12 +2,16 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice'
 
 
 export const SignIn = () => {
   const[formData, setFormData] = useState({})
-  const[errorMessage, setErrorMessage] = useState(null)
-  const[loading, setLoading] = useState('')
+  // const[errorMessage, setErrorMessage] = useState(null)
+  // const[loading, setLoading] = useState('')
+  const{loading, errorMessage} = useSelector(state => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
@@ -17,11 +21,12 @@ export const SignIn = () => {
     
     e.preventDefault();
     if ( !formData.email || !formData.password) {
-      return setErrorMessage("Please fill out all fields")
+      return dispatch(signInFailure('Please Fill Up The Form'))
     }
     try{
-      setLoading(true);
-      setErrorMessage(null)
+      // setLoading(true);
+      // setErrorMessage(null)
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: {'Content-Type' : 'application/json'},
@@ -29,14 +34,15 @@ export const SignIn = () => {
       })
       const data = await res.json();
       if(data.sucess === false){
-         return setErrorMessage(data.message)
+         dispatch(signInFailure(data.message))
         }
-        setLoading(false);
+        // setLoading(false);
         if(res.ok){
+          dispatch(signInSuccess(data))
           navigate('/')
         }
     }catch(err){
-
+      dispatch(signInFailure(err.message))
     }
   }
   return (
